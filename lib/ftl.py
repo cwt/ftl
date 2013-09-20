940fd7e9762c43ac8d128b264394dfd716050389
Chaiwat Suttipongsakul <cwt@bashell.com>
2013-09-20 22:13:55 +0700

    Add md5 for digest method.

--- lib/ftl.py
+++ lib/ftl.py
@@ -3,9 +3,9 @@ import hashlib
 from mmap import mmap
 
 def digest(filename, method='sha256'):
-    if method not in ('sha1', 'sha224', 'sha256', 'sha384', 'sha512'):
+    if method not in ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'):
         raise ValueError(
-            'Supported methods: sha1, sha224, sha256, sha384, sha512'
+            'Supported methods: md5, sha1, sha224, sha256, sha384, sha512'
         )
     h = getattr(hashlib, method)
     f = open(filename, 'r+b')
